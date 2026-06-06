2e66e7141a4fa3489cc3a8aac60a7b5f5144b5fd
WiteckaJ <293142@student.pwr.edu.pl>
2026-06-06 22:47:53 +0200

    Dodana linijka do wpisywania

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,5 +1,7 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout, QLineEdit
 from PySide6.QtCore import Qt
+
+import sys
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -11,12 +13,17 @@ class MainWindow(QMainWindow):
         widget = QWidget()
 
         label1 = QLabel("Kalkulator Macierzy")
-        label1.setAlignment(Qt.AlignTop)
-        label1.setAlignment(Qt.AlignHCenter)
+        label1.setAlignment(Qt.AlignTop | Qt.AlignHCenter)
+
         layout = QVBoxLayout()
         layout.addWidget(label1)
-       
         widget.setLayout(layout)
+
+        self.inputbox = QLineEdit()
+        self.inputbox.setPlaceholderText("Wpisz komende np det[(1,2),(3,4)]")
+        layout.addWidget(self.inputbox)
+        
+
 
         self.setCentralWidget(widget)
 
