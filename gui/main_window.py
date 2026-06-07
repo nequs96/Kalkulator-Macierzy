c0d1d133b00cfb0202b5bc6b330ff6306147018c
WiteckaJ <293142@student.pwr.edu.pl>
2026-06-07 16:50:18 +0200

    Dodane przyciski i pole na wynik

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,4 +1,6 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout, QLineEdit
+from PySide6.QtWidgets import (
+    QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout, QLineEdit,
+    QPushButton, QTextEdit )
 from PySide6.QtCore import Qt
 
 import sys
@@ -15,13 +17,34 @@ class MainWindow(QMainWindow):
         label1 = QLabel("Kalkulator Macierzy")
         label1.setAlignment(Qt.AlignTop | Qt.AlignHCenter)
 
+
         layout = QVBoxLayout()
         layout.addWidget(label1)
         widget.setLayout(layout)
 
+        examples = QLabel("""
+                          det (wyznacznik) 
+                          add (dodawanie) 
+                          tr (slad)""")
+        layout.addWidget(examples)
+        examples.setAlignment(Qt.AlignLeft)
+
+        button1 = QPushButton("oblicz")
+        layout.addWidget(button1)
+
+        button2 = QPushButton("wyczysc")
+        layout.addWidget(button2)
+
         self.inputbox = QLineEdit()
         self.inputbox.setPlaceholderText("Wpisz komende np det[(1,2),(3,4)]")
         layout.addWidget(self.inputbox)
+
+        result = QLabel("Wynik: ")
+        result.setAlignment(Qt.AlignTop | Qt.AlignLeft)
+        layout.addWidget(result)
+
+        self.result_label = QLabel("")
+        layout.addWidget(self.result_label)
         
 
 
